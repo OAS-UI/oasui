d6e4bfa74b262c36d005a7599a93d3a6a0d70c81
Jorge Felico <jorgefelico@gmail.com>
2023-09-30 16:29:48 -0400

    Initial commit

---
+++ packages/stencil/stencil.config.ts
@@ -0,0 +1,36 @@
+import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
+import { angularOutputTarget } from '@stencil/angular-output-target';
+
+export const config: Config = {
+  namespace: 'OASUI',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+    reactOutputTarget({
+      componentCorePackage: '@oasui/web',
+      proxiesFile: '../react/lib/components/stencil-generated/index.ts',
+    }),
+    angularOutputTarget({
+      componentCorePackage: '@oasui/web',
+      outputType: 'component',
+      directivesProxyFile: '../angular/projects/component-library/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../angular/projects/component-library/src/lib/stencil-generated/index.ts',
+    }),
+  ],
+  testing: {
+    browserHeadless: "new",
+  },
+};
